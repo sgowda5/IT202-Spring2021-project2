fa4a2b1f2cea08f6da63b352700ad513b7a1a267
Surya Gowda <sgowda5@uic.edu>
2021-03-30 02:26:30 +0000

    created PWA

---
+++ sw.js
@@ -0,0 +1,43 @@
+let CACHE_NAME = 'Project2-IT-202';
+let urlsToCache = [
+  "./",
+  "./images/background.jpg",
+  "./images/cat.svg",
+  "./images/cheese.svg",
+  "./images/mouse.svg",
+  "./images/icons/icon.png"
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+    console.log('Opened cache');
+    return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+    if (response) {
+      return response;
+    }
+     return fetch(event.request).then(
+     function(response) {
+        if(!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        var responseToCache = response.clone();
+         caches.open(CACHE_NAME)
+           .then(function(cache) {
+           cache.put(event.request, responseToCache);
+         });
+        return response;
+      }
+      );
+      })
+    );
+});
