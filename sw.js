c5d14f0abf67fed258c2e7dd6349f3a35f7ef3cc
Surya Gowda <sgowda5@uic.edu>
2021-03-30 04:27:53 +0000

    final commit

--- sw.js
+++ sw.js
@@ -8,36 +8,37 @@ let urlsToCache = [
   "./images/icons/icon.png"
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then((cache) => {
-    console.log('Opened cache');
-    return cache.addAll(urlsToCache);
-    })
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-    .then((response) => {
-    if (response) {
-      return response;
-    }
-     return fetch(event.request).then(
-     (response) => {
-        if(!response || response.status !== 200 || response.type !== 'basic') {
+      .then(function(response) {
+        if (response) {
           return response;
         }
-        var responseToCache = response.clone();
-         caches.open(CACHE_NAME)
-           .then((cache) => {
-           cache.put(event.request, responseToCache);
-         });
-        return response;
-      }
-      );
+        return fetch(event.request).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
       })
     );
 });
